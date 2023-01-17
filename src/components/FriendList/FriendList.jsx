afe6a15cf0763686a2802a964ad39843d4bdd5c9
IraGubish2022 <iragubish@gmail.com>
2023-01-17 12:43:36 +0200

    17.01.23-12.45

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,32 @@
+import FriendListItem from 'components/FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
+import style from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={style.friendList}>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.defaultProps = {
+  friends: [],
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
